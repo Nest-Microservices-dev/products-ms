c8d9b0ce2a2f71653a0804eb7325619da400b4d1
cuter97 <aguscastets@gmail.com>
2024-05-04 16:20:12 -0300

    Add nats

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,9 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port
+        servers: envs.natsServers,
       }
     }
   );
@@ -26,7 +26,7 @@ async function bootstrap() {
   );
 
   await app.listen();
-  logger.log(`Products Microservice running on port ${ envs.port }`);
+  logger.log(`Products Microservice running on port ${envs.port}`);
 
 }
 bootstrap();
